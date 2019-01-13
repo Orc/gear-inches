de010de62ab728f3fb6bac99d7178d2819b740a2
david parsons <orc@pell.portland.or.us>
2019-01-13 01:55:28 -0800

    update the makefile to build gcalc

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 OBJS=gi.o
+LIBS=-lm
 
-gi:  $(OBJS)
-	cc -o gi $(OBJS) -lm
+all:  gi gcalc
+
+gi:  gi.o
+	$(CC) -o gi gi.o $(LIBS)
+
+gcalc:  gcalc.o uncgi.o
+	$(CC) -o gcalc gcalc.o uncgi.o $(LIBS)
+
+uncgi.o: uncgi.c
+	$(CC) -c -DLIBRARY -DNO_QUERY_OK uncgi.c
 
 clean spotless:
-	rm -f $(OBJS) gi
+	rm -f *.o gi gcalc
---
+++ gcalc.c
@@ -0,0 +1,314 @@
+/*
+ * gear inch calculator
+ *
+ * usage: gi wheel tire chainring{,chainring} cog{,cog}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <libgen.h>
+#include <stdarg.h>
+#include <inttypes.h>
+#include <limits.h>
+
+#define if(x)	if((x))
+
+
+char *usage = "wheel tire_size chainring{,chainring} cog{,cog}";
+
+int diameter;
+int tire;
+
+#define MAX_CHAINRINGS 10
+#define MAX_COGS 20
+
+int chainrings[MAX_CHAINRINGS];
+int cogs[MAX_CHAINRINGS];
+
+int nr_chainrings;
+int nr_cogs;
+
+
+int web = 0;
+
+struct { int diameter; char *alias; } diameters[] = {
+    { 630, "27" },
+    { 622, "700c" },
+    { 590, "650a" },
+    { 487, "700d" },
+    { 584, "650b" },
+    { 584, "27.5" },
+    { 571, "650c" },
+    { 559, "26" },
+} ;
+#define NR_DIAMETERS (sizeof diameters / sizeof diameters[0])
+
+
+int
+gi(int chainring, int cog)
+{
+    float mm_gain = (diameter + tire);
+
+    mm_gain *= chainring;
+    mm_gain /= cog;
+
+    return mm_gain / 25.4;
+}
+
+
+char errorbuf[2048];
+int errorsize;
+
+#define EEND	(errorbuf + errorsize)
+#define ESIZE	(sizeof errorbuf - errorsize)
+
+void
+noerrors()
+{
+    memset(errorbuf, 0, sizeof errorbuf);
+    errorsize = 0;
+}
+
+void
+error(char *fmt, ...)
+{
+    va_list ptr;
+
+    va_start(ptr,fmt);
+
+    errorsize += snprintf(EEND, ESIZE, "<p>");
+    errorsize += vsnprintf(EEND, ESIZE, fmt, ptr);
+    errorsize += snprintf(EEND, ESIZE, "</p>");
+    
+    va_end(ptr);
+}
+
+
+int
+has_errors()
+{
+    return errorsize > 0;
+}
+
+
+int
+number(char *s)
+{
+    char *end;
+    long value = strtoul(s, &end, 10);
+
+    if ( end == s || *end != 0 )
+	return -1;
+
+    return (int) value;
+}
+
+
+void
+populate()
+{
+    char *val;
+    char variable[80];
+    int i, teeth;
+
+    diameter = 0;
+    tire = 0;
+    memset(chainrings, 0, sizeof chainrings);
+    memset(cogs, 0, sizeof cogs);
+    
+    noerrors();
+    
+    if ( !getenv("WWW_cgi") )
+	return;
+
+    if ( val = getenv("WWW_diameter") ) {
+	for (i = 0; i < NR_DIAMETERS; i++ )
+	    if ( strcasecmp(val, diameters[i].alias) == 0 ) {
+		diameter = diameters[i].diameter;
+		break;
+	    }
+
+	if ( diameter == 0 ) {
+	    diameter = number(val);
+	    if ( diameter <= 0 )
+		error("wheel diameter [%s]?", val);
+	}
+    }
+
+    if ( val = getenv("WWW_tire") ) {
+	tire = number(val);
+
+	if ( tire <= 0 )
+	    error("tire size [%s]?", val);
+    }
+    
+    for ( i = 0; i < MAX_CHAINRINGS; i++ ) {
+	sprintf(variable, "WWW_ring%d", i);
+
+	if ( val = getenv(variable) ) {
+	    if ( (teeth = number(val)) <= 0 )
+		error("chainring [%s]?", val);
+	    chainrings[i] = teeth;
+	}
+    }
+
+    for ( i = MAX_CHAINRINGS; i > 0 && chainrings[i-1] == 0; --i )
+	;
+    nr_chainrings = i;
+
+    for ( i = 0; i < MAX_COGS; i++ ) {
+	sprintf(variable, "WWW_cog%d", i);
+
+	if ( val = getenv(variable) ) {
+	    if ( (teeth = number(val)) <= 0 )
+		error("cog [%s]?", val);
+	    cogs[i] = teeth;
+	}
+    }
+
+    for ( i = MAX_COGS; i > 0 && cogs[i-1] == 0; --i )
+	;
+    nr_cogs = i;
+}
+
+
+void
+cell(int bold, char *align, char *fmt, ...)
+{
+    va_list ptr;
+
+    va_start(ptr, fmt);
+
+    printf("    <t%c", bold ? 'h' : 'd');
+    if ( align )
+	printf(" align=\"%s\"", align);
+    putchar('>');
+    vprintf(fmt, ptr);
+    printf("</t%c>\n", bold ? 'h' : 'd');
+    va_end(ptr);
+}
+
+void
+row()
+{
+    puts("<tr>");
+}
+
+void
+end()
+{
+    puts("</tr>");
+}
+
+
+void
+input(char *name, int size, char *value)
+{
+    printf("    <input type=\"text\" name=\"%s\", size=\"%d\" value=\"%s\" />\n",
+	    name, size, value ? value : "");
+}
+ 
+
+void
+show_form()
+{
+    int i, j;
+    char name[80];
+
+    printf("Content-Type: text/html\n\n");
+
+
+    puts("<html>");
+    puts("<head><title>Gear Inch Calculator</title></head>");
+
+    puts("<body>");
+
+    puts("<form method=\"post\" action=\"gcalc.cgi\">");
+    puts("<table>");
+    row();
+    cell(1, "right", "Wheel Diameter");
+    puts("    <td>");
+    input("diameter", 6, getenv("WWW_diameter"));
+    puts("    </td>");
+    end();
+
+    row();
+    cell(1, "right", "Tire Size");
+    puts("    <td>");
+    input("tire", 3, getenv("WWW_tire"));
+    puts("    </td>");
+    end();
+
+    row();
+    cell(1, "right", "Chainring(s)");
+    puts("    <td>");
+    for (i=1; i<=3; i++) {
+	sprintf(name, "WWW_ring%d", i);
+	input(name+4, 2, getenv(name));
+    }
+    puts("    </td>");
+    end();
+
+    row();
+    cell(1, "right", "Cog(s)");
+    puts("    <td>");
+    for (i=1; i<=12; i++) {
+	sprintf(name, "WWW_cog%d", i);
+	input(name+4, 2, getenv(name));
+    }
+    puts("    </td>");
+    end();
+
+    row();
+    puts("    <td></td>");
+    puts("    <th align=\"left\">");
+    puts("    <input type=\"hidden\" id=\"cgi\" name=\"cgi\" value=\"T\" />");
+    puts("    <input type=\"submit\" value=\"tell me my gear inches\"/>");
+    puts("    </th>");
+    end();
+
+}
+
+
+int /* Siiiiiigh, fine */
+main(argc, argv)
+char **argv;
+{
+    int i, j;
+    char *arg;
+    char strval[10];
+
+    void uncgi();
+
+    uncgi();
+
+    populate();
+    show_form();
+
+    if ( has_errors() ) {
+	puts("</table>");
+	puts(errorbuf);
+    }
+    else  {
+	for ( i=0; i < nr_cogs; i++ ) {
+	    row();
+	    sprintf(strval, "%9d", cogs[i]);
+	    cell(1, 0, cogs[i] > 0 ? strval : "");
+
+	    for ( j = 0; j < nr_chainrings; j++ ) {
+		if ( chainrings[j] && cogs[i] )
+		sprintf(strval, "%9d", gi(chainrings[j], cogs[i]));
+		cell(0, 0, strval);
+	    }
+	    end();
+	}
+	puts("</table>");
+    }
+    puts("</form>");
+    puts("</body>");
+    puts("</html>");
+    exit(0);
+}
