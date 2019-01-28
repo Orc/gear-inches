dfaca9354bfa5dd9e5461370e413446dd2adb7e5
david parsons <orc@pell.portland.or.us>
2019-01-27 20:41:14 -0800

    add a bunch of french sizes to wheel diameters, plus parse 'inch' and '"' (29,26,24,20) and 'mm' suffixes.

--- functions.c
+++ functions.c
@@ -3,21 +3,51 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <errno.h>
+#include <ctype.h>
 
 #include "functions.h"
 
-struct { int diameter; char *alias; } diameters[] = {
-    { 630, "27" },
-    { 622, "700c" },
-    { 590, "650a" },
-    { 587, "700d" },
-    { 584, "650b" },
-    { 584, "27.5" },
-    { 571, "650c" },
-    { 559, "26" },
-};
+struct _diameter { int diameter; char *alias; } ;
 
+struct _diameter diameters[] = {
+    { 642, "700a" },
+    { 635, "700b" },
+    { 622, "700c" },
+    { 587, "700d" },
+    { 590, "650a" },
+    { 584, "650b" },
+    { 571, "650c" },
+    { 540, "600a" },
+    { 490, "550a" },
+    { 440, "500a" },
+    { 390, "450a" },
+    { 340, "400a" },
+    { 584, "27.5" },
+    { 584, "27.5\"" },
+};
 #define NR_DIAMETERS (sizeof diameters / sizeof diameters[0])
+
+struct _diameter inches[] = {
+    { 622, "29" },
+    { 630, "27" },
+    { 559, "26" },
+    { 520, "24" },
+    { 451, "20" },
+};
+#define NR_INCHES (sizeof inches / sizeof inches[0])
+
+
+static int
+lookup(char *s, struct _diameter dia[], int nr_dia)
+{
+    int i;
+    
+    for ( i=nr_dia-1; i >= 0; --i)
+	if ( strcasecmp(s, dia[i].alias) == 0 )
+	    return i;
+
+    return EOF;
+}
 
 
 int diameter, tire;
@@ -28,16 +58,35 @@ wheel_diameter(char *s)
 {
     int i;
     char *e;
+    char *sfx;
 
 
-    for ( i=0; i<NR_DIAMETERS; i++ )
-	if ( strcasecmp(s, diameters[i].alias) == 0 )
-	    return diameters[i].diameter;
+    if ( (i = lookup(s, diameters, NR_DIAMETERS)) != EOF )
+	return diameters[i].diameter;
 
     i = strtoul(s,&e,10);
 
-    if ( *e )					/* trailing garbage on token */
-	return EOF;
+    if ( *e ) {
+	for (sfx=e; isspace(*sfx); ++sfx)
+	    ;
+	
+	if ( strcmp(sfx, "in") == 0 ) {
+	    *e = 0;
+
+	    if ( (i = lookup(s, inches, NR_INCHES)) != EOF )
+		return inches[i].diameter;
+	}
+	else if ( strcmp(sfx, "\"") == 0 ) {
+	    *e = 0;
+
+	    if ( (i=lookup(s,inches, NR_INCHES)) != EOF )
+		return inches[i].diameter;
+	}
+	else if ( strcmp(sfx, "mm") != 0 ) {
+	    /* trailing garbage on token */
+	    return EOF;
+	}
+    }
 
     if ( i == ULONG_MAX && errno == ERANGE )	/* overflow */
 	return EOF;
